bb0614d945c38114bcfcb22babc52c9ae2d9a2ef
UriMV23 <isaidvazquez.2309@gmail.com>
2025-05-20 00:26:55 -0600

    Update

--- controllers/ineController.js
+++ controllers/ineController.js
@@ -1,28 +1,124 @@
 const db = require('../db');
 
-exports.guardarDatos = (req, res) => {
-  const {
-    nombre,
-    apellido_paterno,
-    apellido_materno,
-    clave_elector,
-    curp,
-    anio_registro,
-    vigencia,
-    domicilio
-  } = req.body;
+// Obtener por ID o CURP
+exports.getByIdOrCurp = (req, res) => {
+  const { id, curp } = req.query;
 
-  const query = `
-    INSERT INTO personas (nombre, apellido_paterno, apellido_materno, clave_elector, curp, anio_registro, vigencia, domicilio)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+  let sql = `
+    SELECT * FROM personas p
+    LEFT JOIN direcciones d ON p.curp = d.curp
+    LEFT JOIN datos_ine i ON p.curp = i.curp
+  `;
+  let params = [];
 
-  db.query(query, [nombre, apellido_paterno, apellido_materno, clave_elector, curp, anio_registro, vigencia, domicilio],
-    (err, results) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error al guardar los datos' });
-      } else {
-        res.status(201).json({ mensaje: 'Datos guardados correctamente' });
-      }
+  if (id) {
+    sql += ' WHERE p.id = ?';
+    params.push(id);
+  } else if (curp) {
+    sql += ' WHERE p.curp = ?';
+    params.push(curp);
+  }
+
+  db.query(sql, params, (err, results) => {
+    if (err) return res.status(500).json({ error: err });
+    res.json(results);
+  });
+};
+
+// Obtener todos los registros
+exports.getAll = (req, res) => {
+  const sql = `
+    SELECT * FROM personas p
+    LEFT JOIN direcciones d ON p.curp = d.curp
+    LEFT JOIN datos_ine i ON p.curp = i.curp
+  `;
+  db.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: err });
+    res.json(results);
+  });
+};
+
+// Insertar en las 3 tablas
+exports.insertAll = (req, res) => {
+  const { persona, direccion, datosIne } = req.body;
+
+  db.beginTransaction((err) => {
+    if (err) return res.status(500).json({ error: err });
+
+    const insertPersona = `INSERT INTO personas SET ?`;
+    db.query(insertPersona, persona, (err) => {
+      if (err) return db.rollback(() => res.status(500).json({ error: err }));
+
+      const insertDireccion = `INSERT INTO direcciones SET ?`;
+      db.query(insertDireccion, direccion, (err) => {
+        if (err) return db.rollback(() => res.status(500).json({ error: err }));
+
+        const insertDatosIne = `INSERT INTO datos_ine SET ?`;
+        db.query(insertDatosIne, datosIne, (err) => {
+          if (err) return db.rollback(() => res.status(500).json({ error: err }));
+
+          db.commit((err) => {
+            if (err) return db.rollback(() => res.status(500).json({ error: err }));
+            res.json({ message: 'Datos insertados correctamente' });
+          });
+        });
+      });
     });
+  });
+};
+
+// Eliminar por CURP
+exports.deleteByCurp = (req, res) => {
+  const { curp } = req.params;
+
+  db.beginTransaction((err) => {
+    if (err) return res.status(500).json({ error: err });
+
+    const deleteDatosIne = `DELETE FROM datos_ine WHERE curp = ?`;
+    db.query(deleteDatosIne, [curp], (err) => {
+      if (err) return db.rollback(() => res.status(500).json({ error: err }));
+
+      const deleteDirecciones = `DELETE FROM direcciones WHERE curp = ?`;
+      db.query(deleteDirecciones, [curp], (err) => {
+        if (err) return db.rollback(() => res.status(500).json({ error: err }));
+
+        const deletePersona = `DELETE FROM personas WHERE curp = ?`;
+        db.query(deletePersona, [curp], (err) => {
+          if (err) return db.rollback(() => res.status(500).json({ error: err }));
+
+          db.commit((err) => {
+            if (err) return db.rollback(() => res.status(500).json({ error: err }));
+            res.json({ message: 'Datos eliminados correctamente' });
+          });
+        });
+      });
+    });
+  });
+};
+
+// Actualizar por CURP
+exports.updateByCurp = (req, res) => {
+  const { curp } = req.params;
+  const { persona, direccion, datosIne } = req.body;
+
+  db.beginTransaction((err) => {
+    if (err) return res.status(500).json({ error: err });
+
+    db.query('UPDATE personas SET ? WHERE curp = ?', [persona, curp], (err) => {
+      if (err) return db.rollback(() => res.status(500).json({ error: err }));
+
+      db.query('UPDATE direcciones SET ? WHERE curp = ?', [direccion, curp], (err) => {
+        if (err) return db.rollback(() => res.status(500).json({ error: err }));
+
+        db.query('UPDATE datos_ine SET ? WHERE curp = ?', [datosIne, curp], (err) => {
+          if (err) return db.rollback(() => res.status(500).json({ error: err }));
+
+          db.commit((err) => {
+            if (err) return db.rollback(() => res.status(500).json({ error: err }));
+            res.json({ message: 'Datos actualizados correctamente' });
+          });
+        });
+      });
+    });
+  });
 };
--- routes/ine.js
+++ routes/ine.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const ineController = require('../controllers/ineController');
+const controller = require('../controllers/ineController');
 
-router.post('/guardar', ineController.guardarDatos);
+router.get('/consulta', controller.getByIdOrCurp);
+router.get('/todos', controller.getAll);
+router.post('/insertar', controller.insertAll);
+router.delete('/eliminar/:curp', controller.deleteByCurp);
+router.put('/actualizar/:curp', controller.updateByCurp);
 
 module.exports = router;
